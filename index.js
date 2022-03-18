bc1beecb99991c5ead968ef595382ec5e533561d
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-03-18 10:50:24 +0000

    updated image files

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post(
     const tempPath = req.file.path;
     const filename = req.file.originalname;
     const targetPath = path.join(__dirname, "./public/images/" + filename);
-    if (path.extname(req.file.originalname).toLowerCase() === ".jpg") {
+    if (path.extname(filename).toLowerCase() === ".jpg") {
       fs.rename(tempPath, targetPath, (err) => {
         if (err) return handleError(err, res);
         res.status(200).render("layout", {
